978b6db083fdd845df3f464a20291da91d5d703e
cohinteractive <charles@ohinteractive.co.nz>
2025-07-29 11:17:14 +1200

    log: update

--- CODEXLOG_CURRENT.md
+++ CODEXLOG_CURRENT.md
@@ -1,3 +1,4 @@
 [2507282059][431821][FTR] Initialize Flutter project structure
 [2507282231][d81bbc][FTR] Implement main window with menu bar
 [2507282257][57b6fb][REF] Centralize menu actions
+[2507282304][44ce88][FTR][REF] Add MenuActions constants and refactor handler
---
+++ lib/menu_constants.dart
@@ -0,0 +1,10 @@
+class MenuActions {
+  static const String openJson = 'openJson';
+  static const String openVault = 'openVault';
+  static const String exportPlaceholder1 = 'exportPlaceholder1';
+  static const String exportPlaceholder2 = 'exportPlaceholder2';
+  static const String exitApp = 'exitApp';
+  static const String viewContext = 'viewContext';
+  static const String selectModelGpt = 'selectModelGpt';
+  static const String selectModelGemini = 'selectModelGemini';
+}
